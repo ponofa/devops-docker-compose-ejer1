2106ca162c688b9fa19094fbda939c05875c43a6
Polivio Onofa <ponofa@14demarzo.fin.ec>
2026-05-07 17:07:18 -0500

    First upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# devops-docker-compose-ejer1
+Ejercicio 1 para practica docker compose
---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/api/login", (req, res) => {
+  // Demo: login fijo
+  console.log("📥 [LOGIN] Petición recibida:");
+  console.log("Headers:", req.headers);
+  console.log("Body:", req.body);
+
+  const { user, pass } = req.body;
+  if (user === "admin" && pass === "1234") {
+    console.log("❌ Credenciales inválidas:", { user, pass });
+    return res.json({ ok: true, token: "fake-token-123" });
+  }
+  console.log("✅ Login exitoso:", { user });
+  return res.status(401).json({ ok: false, message: "Credenciales inválidas" });
+});
+
+app.get("/api/dashboard", (req, res) => {
+  // Demo: datos ficticios
+  res.json({
+    ok: true,
+    stats: {
+      users: 42,
+      sales: 1234,
+      uptime: "99.9%"
+    }
+  });
+});
+
+// app.listen(3000, () => console.log("API escuchando en puerto 3000"));
+app.listen(3000, "0.0.0.0", () => {
+  console.log("API escuchando en puerto 3000");
+});
+
